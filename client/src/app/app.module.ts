82daa513ef4c4d85ed5c734325c1aa119a3147c4
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-04-12 14:48:35 +0200

    use english naming convention

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { InventarComponent } from './inventar/inventar.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InventarComponent
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
