0c4d318bc17245ff741dd919489410ac7909509d
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-04-12 14:47:00 +0200

    basic angular setup, add inventar page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { InventarComponent } from './inventar/inventar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventarComponent
   ],
   imports: [
     BrowserModule,
