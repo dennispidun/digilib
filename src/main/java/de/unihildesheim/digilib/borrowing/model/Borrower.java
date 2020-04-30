e34ae5675f8fe4c01e44902ac6c5e3fa8a4a931d
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-04-30 19:02:07 +0200

    fix

--- src/main/java/de/unihildesheim/digilib/borrowing/model/Borrower.java
+++ src/main/java/de/unihildesheim/digilib/borrowing/model/Borrower.java
@@ -1,13 +1,12 @@
 package de.unihildesheim.digilib.borrowing.model;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @Data
