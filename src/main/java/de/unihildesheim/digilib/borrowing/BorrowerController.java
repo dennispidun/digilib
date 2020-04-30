2b897d4b1b32ff9486ce4fa1e362633c838a772d
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-04-30 19:29:54 +0200

    USE TYPEAHEAD AND DONT REMOVE ME :(

--- src/main/java/de/unihildesheim/digilib/borrowing/BorrowerController.java
+++ src/main/java/de/unihildesheim/digilib/borrowing/BorrowerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/borrower")
@@ -22,10 +23,33 @@ public class BorrowerController {
 
     @GetMapping
     public List<Borrower> getBorrowers(@RequestParam(required = false) String search,
-                                       @RequestParam int pageNo,
-                                       @RequestParam int pageSize) {
+                                       @RequestParam(required = false) Integer pageNo,
+                                       @RequestParam(required = false) Integer pageSize) {
+        if (search != null && search.length() > 0) {
+            return findBorrowersMatching(search);
+        }
+
         return borrowerRepository.findAll(PageRequest.of(pageNo, pageSize)).toList();
     }
+
+    private List<Borrower> findBorrowersMatching(@RequestParam(required = false) String search) {
+        String firstname = search;
+        String lastname = search;
+
+        if (search.contains(" ")) {
+            firstname = search.substring(0, search.lastIndexOf(" "));
+            lastname = search.substring(search.lastIndexOf(" "));
+        }
+
+        List<Borrower> searchBorrowersFirst = borrowerRepository
+                .findAllByFirstnameIgnoreCaseContainingOrLastnameIgnoreCaseContaining(firstname, lastname);
+        List<Borrower> searchBorrowersSecond = borrowerRepository
+                .findAllByFirstnameIgnoreCaseContainingOrLastnameIgnoreCaseContaining(lastname, firstname);
+        searchBorrowersFirst.addAll(searchBorrowersSecond);
+        return searchBorrowersFirst.stream().distinct().collect(Collectors.toList());
+    }
+
+
 
     @PatchMapping(value = "/{id}/teacher", consumes = MediaType.TEXT_PLAIN_VALUE)
     public Borrower setTeacher(@PathVariable("id") long id, @RequestBody String teacher) {
