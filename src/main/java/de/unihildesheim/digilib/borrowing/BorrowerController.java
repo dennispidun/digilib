c781311d4d65652c1bc0bc97d9e8569ef16c9bb7
Freenk <tobias.wenck@t-online.de>
2020-04-30 19:23:21 +0200

    fixed my last merge lol

--- src/main/java/de/unihildesheim/digilib/borrowing/BorrowerController.java
+++ src/main/java/de/unihildesheim/digilib/borrowing/BorrowerController.java
@@ -1,14 +1,12 @@
 package de.unihildesheim.digilib.borrowing;
 
 import de.unihildesheim.digilib.borrowing.model.Borrower;
+import de.unihildesheim.digilib.borrowing.model.Borrowing;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/borrower")
@@ -24,38 +22,21 @@ public class BorrowerController {
 
     @GetMapping
     public List<Borrower> getBorrowers(@RequestParam(required = false) String search,
-                                       @RequestParam(required = false) Integer pageNo,
-                                       @RequestParam(required = false) Integer pageSize) {
-        if (search != null && search.length() > 0) {
-            return findBorrowersMatching(search);
-        }
-
+                                       @RequestParam int pageNo,
+                                       @RequestParam int pageSize) {
         return borrowerRepository.findAll(PageRequest.of(pageNo, pageSize)).toList();
     }
-
-    private List<Borrower> findBorrowersMatching(@RequestParam(required = false) String search) {
-        String firstname = search;
-        String lastname = search;
-
-        if (search.contains(" ")) {
-            firstname = search.substring(0, search.lastIndexOf(" "));
-            lastname = search.substring(search.lastIndexOf(" "));
-        }
-
-        List<Borrower> searchBorrowersFirst = borrowerRepository
-                .findAllByFirstnameIgnoreCaseContainingOrLastnameIgnoreCaseContaining(firstname, lastname);
-        List<Borrower> searchBorrowersSecond = borrowerRepository
-                .findAllByFirstnameIgnoreCaseContainingOrLastnameIgnoreCaseContaining(lastname, firstname);
-        searchBorrowersFirst.addAll(searchBorrowersSecond);
-        return searchBorrowersFirst.stream().distinct().collect(Collectors.toList());
-    }
-
 
     @PatchMapping(value = "/{id}/teacher", consumes = MediaType.TEXT_PLAIN_VALUE)
     public Borrower setTeacher(@PathVariable("id") long id, @RequestBody String teacher) {
         Borrower borrower = borrowerRepository.findById(id).orElseThrow(() -> new BorrowerNotFoundException(id));
         borrower.setTeacher(teacher.equals("true"));
         return borrowerRepository.save(borrower);
+    }
+
+    @GetMapping(value = "/{id}/unreturned")
+    public List<Borrowing> getUnreturned(@PathVariable("id") long id) {
+        return borrowingRepository.getBorrowingByBorrower_IdAndReturnedOnIsNull(id);
     }
 
 }
