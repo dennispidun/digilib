45047c912c887625e9a74aef10d1475be39b27c4
Freenk <tobias.wenck@t-online.de>
2020-04-30 23:32:17 +0200

    use custom login even if application is built

--- src/main/java/de/unihildesheim/digilib/user/WebSecurityConfiguration.java
+++ src/main/java/de/unihildesheim/digilib/user/WebSecurityConfiguration.java
@@ -24,7 +24,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .httpBasic();
     }
