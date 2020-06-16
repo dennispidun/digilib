e9e68d35447c8c206e276c9e077f2fddf99cf583
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-06-16 19:05:03 +0200

    wip changes

--- src/main/java/de/unihildesheim/digilib/user/UserInitializer.java
+++ src/main/java/de/unihildesheim/digilib/user/UserInitializer.java
@@ -17,21 +17,27 @@ public class UserInitializer {
 
     @PostConstruct
     public void init(){
-        repository.deleteAll();
-        User entity = new User();
-        entity.setEnabled(true);
-        entity.setUsername("admin");
-        entity.setFirstname("Erika");
-        entity.setLastname("Musterfrau");
-        entity.setPassword(passwordEncoder.encode("password"));
-        entity.setRole(Role.ADMIN);
-        repository.save(entity);
-        User entity2 = new User();
-        entity2.setEnabled(true);
-        entity2.setUsername("user");
-        entity2.setFirstname("Max");
-        entity2.setLastname("Mustermann");
-        entity2.setPassword(passwordEncoder.encode("1234"));
-        repository.save(entity2);
+        if (!repository.findUserByUsername("admin").isPresent()) {
+            User entity = new User();
+            entity.setEnabled(true);
+            entity.setUsername("admin");
+            entity.setFirstname("Erika");
+            entity.setLastname("Musterfrau");
+            entity.setPassword(passwordEncoder.encode("password"));
+            entity.setRole(Role.ADMIN);
+            repository.save(entity);
+        }
+
+        if (!repository.findUserByUsername("user").isPresent()) {
+            User entity2 = new User();
+            entity2.setEnabled(true);
+            entity2.setUsername("user");
+            entity2.setFirstname("Max");
+            entity2.setLastname("Mustermann");
+            entity2.setPassword(passwordEncoder.encode("1234"));
+            repository.save(entity2);
+        }
+
+
     }
 }
