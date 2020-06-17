4d7c5d3fccd92a020630422e70f32c8c34957dcb
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-06-17 15:29:35 +0200

    redirect user on forbidden

--- client/src/app/http.interceptor.ts
+++ client/src/app/http.interceptor.ts
@@ -29,6 +29,9 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(request).pipe( tap(() => {},
       (err: any) => {
         if (err instanceof HttpErrorResponse) {
+          if (err.status === 403) {
+            this.router.navigate(["dashboard"]);
+          }
           if (err.status !== 401) {
             return;
           }
