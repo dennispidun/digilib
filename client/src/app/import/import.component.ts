7e4ef6c0976cf50156d49e44e3fa48d9bc933909
tobi <cmd02@t-online.de>
2020-06-18 13:24:11 +0200

    hides report if empty; makes import frontend more responsive; optimizes frontend import code

--- client/src/app/import/import.component.ts
+++ client/src/app/import/import.component.ts
@@ -63,63 +63,74 @@ export class ImportComponent implements OnInit {
 
   printReport() {
     this.result += new Date().toLocaleString() + "\n";
-    if (this.report.successfull > 0) {
-      this.result += "Es wurde" + (this.report.successfull > 1 ? "n " + this.report.successfull + " Bücher" : " ein Buch")
-        + " korrekt importiert.\n";
-    }
-    if (this.report.failed > 0) {
-      this.result += "Es konnte" + (this.report.failed > 1 ? "n " + this.report.failed + " Bücher" : " ein Buch")
-        + " nicht korrekt importiert werden.\n";
-    }
-    this.result += "\n";
-    if (this.report.emptyLines > 0) {
-      this.result += (this.report.emptyLines > 1 ? this.report.emptyLines + " Zeilen" : "Eine Zeile") +
-        " wurde als leer interpretiert.\n\n";
-    }
-    for (const [key, value] of Object.entries(this.report.errs)) {
-      const arr: object[] = value as object[];
-      switch (key) {
-        case "ALREADYEX":
-          this.result += (arr.length > 1 ? arr.length + " Bücher existieren" : "Ein Buch existiert") + " bereits:\n";
-          arr.forEach((element: Book) => {
-            this.result += "#" + element.invnr + " von " + element.author + " mit dem Titel \"" + element.title + "\"\n";
-          })
-          this.result += "\n";
-          break;
-        case "DELIMITERERR":
-          this.result += (arr.length > 1 ? arr.length + " Bücher" : "Ein Buch") + " beinhaltete ein Trennzeichen, welches nicht korrekt verarbeitet werden konnte:\n";
-          this.addArr(arr);
-          break;
-        case "NOTENOUGHINF":
-          this.result += "Bei " + (arr.length > 1 ? arr.length + " Büchern" : "einem Buch") + " wurden nicht genügeng Daten bereitgestellt, um es zu importieren"
-          + " (es werden immer mindestens ein Autor, ein Titel und eine Inventurnummer gefordert):\n";
-          this.addArr(arr);
-          break;
-        case "ENCODINGERR":
-          this.result += "Folgende Probleme wurden mit der Kodierung der Datei(en) festgestellt:\n";
-          this.addArr(arr);
-          break;
-        case "IOEX":
-          this.result += "Folgende Probleme wurden beim Lesen/Schreiben der Datei(en) festgestellt:\n";
-          this.addArr(arr);
-          break;
-        case "FILENOTFOUND":
-          this.result += (arr.length > 1 ? arr.length + " Dateien" : "Eine Datei") + " konnte nicht gefunden werden:\n";
-          arr.forEach((element => {
-            const f: File = element as File;
-            this.result += f.name;
-          }))
-          this.result += "\n";
-          break;
-        case "FOLDEREMPTY":
-          this.result += (arr.length > 1 ? arr.length + " Dateipfade existierten" : "Ein Dateipfad existierte")
-          + "noch nicht, daher konnte dort nichts importiert werden. Der Dateipfad wurde nun erstellt:\n"
-          this.addArr(arr);
-          break;
-        default:
-          this.result += "Ein unbekannter Fehler ist aufgetreten, siehe Konsole für mehr Informationen."
-          console.log(key);
-          console.log(value);
+    const entries = Object.entries(this.report.errs);
+    const success = this.report.successfull;
+    const failed = this.report.failed;
+    const empty = this.report.emptyLines;
+    if (success === 0 && failed === 0 && entries.length === 0) {
+      this.result += "Es gab nichts zu importieren, daher wurde nichts importiert.\n\n"
+    } else {
+      if (success > 0) {
+        this.result += "Es wurde" + (success > 1 ? "n " + success + " Bücher" : " ein Buch")
+          + " korrekt importiert.\n";
+      }
+      if (failed > 0) {
+        this.result += "Es konnte" + (failed > 1 ? "n " + failed + " Bücher" : " ein Buch")
+          + " nicht korrekt importiert werden.\n";
+      }
+      this.result += "\n";
+      if (empty > 0) {
+        this.result += (empty > 1 ? empty + " Zeilen" : "Eine Zeile") +
+          " wurde als leer interpretiert.\n\n";
+      }
+      for (const [key, value] of entries) {
+        const arr: object[] = value as object[];
+        switch (key) {
+          case "ALREADYEX":
+            this.result += (arr.length > 1 ? arr.length + " Bücher existieren" : "Ein Buch existiert") + " bereits:\n";
+            arr.forEach((element: Book) => {
+              this.result += "#" + element.invnr + " von " + element.author + " mit dem Titel \"" + element.title + "\"\n";
+            })
+            this.result += "\n";
+            break;
+          case "DELIMITERERR":
+            this.result += (arr.length > 1 ? arr.length + " Bücher" : "Ein Buch") + " beinhaltete ein Trennzeichen, " +
+              "welches nicht korrekt verarbeitet werden konnte:\n";
+            this.addArr(arr);
+            break;
+          case "NOTENOUGHINF":
+            this.result += "Bei " + (arr.length > 1 ? arr.length + " Büchern" : "einem Buch") + " wurden nicht genügeng" +
+              " Daten bereitgestellt, um es zu importieren (es werden immer mindestens ein Autor, ein Titel und eine " +
+              "Inventurnummer gefordert):\n";
+            this.addArr(arr);
+            break;
+          case "ENCODINGERR":
+            this.result += "Folgende Probleme wurden mit der Kodierung der Datei(en) festgestellt:\n";
+            this.addArr(arr);
+            break;
+          case "IOEX":
+            this.result += "Folgende Probleme wurden beim Lesen/Schreiben der Datei(en) festgestellt:\n";
+            this.addArr(arr);
+            break;
+          case "FILENOTFOUND":
+            this.result += (arr.length > 1 ? arr.length + " Dateien" : "Eine Datei") + " konnte nicht gefunden werden:\n";
+            arr.forEach((element => {
+              const f: File = element as File;
+              this.result += f.name;
+            }))
+            this.result += "\n";
+            break;
+          case "FOLDEREMPTY":
+            this.result += (arr.length > 1 ? arr.length + " Dateipfade existierten" : "Ein Dateipfad existierte")
+              + "noch nicht, daher konnte dort nichts importiert werden. Der Dateipfad wurde nun erstellt:\n"
+            this.addArr(arr);
+            break;
+          default:
+            this.result += "Ein unbekannter Fehler ist aufgetreten, siehe Konsole für mehr Informationen."
+            console.log(key);
+            console.log(value);
+            break;
+        }
       }
     }
   }
